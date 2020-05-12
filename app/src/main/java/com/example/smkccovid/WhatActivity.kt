166dc83867f9f196dc0c88269aae1a23a960f6d7
abelherl <abangabel02@gmail.com>
2020-05-12 17:22:46 +0700

    Updated colors and added new drawables New strings Updated info activity

--- app/src/main/java/com/example/smkccovid/WhatActivity.kt
+++ app/src/main/java/com/example/smkccovid/WhatActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.voela.actrans.AcTrans
 import kotlinx.android.synthetic.main.activity_what.*
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 class WhatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_what)
+
+        OverScrollDecoratorHelper.setUpOverScroll(sv_what)
 
         bt_back.setOnClickListener { buttonBack() }
     }
